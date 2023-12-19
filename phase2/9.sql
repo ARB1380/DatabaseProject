3c21a01f8e1d0256411e3d7ae74cf1cb2a676c65
zhd80 <85179695+zhd80@users.noreply.github.com>
2023-12-19 15:43:14 +0330

    Update 9.sql

--- phase2/9.sql
+++ phase2/9.sql
@@ -1,8 +1,10 @@
+SET SESSION my.vars.parking_Id = 123456789;
+
 SELECT parking_id, COUNT(*) AS car_count
 FROM parking_receipt
-WHERE parking_id = 123456789
+WHERE parking_id = current_setting('my.vars.parking_Id')::int
 GROUP BY parking_id
 HAVING COUNT(*) = (SELECT MAX(temp.count) 
-				   FROM (SELECT COUNT(*) AS count FROM parking_receipt 
-						 WHERE parking_id = 123456789 
-						 GROUP BY parking_id) AS temp)
+   FROM (SELECT COUNT(*) AS count FROM parking_receipt 
+   WHERE parking_id = 123456789 
+   GROUP BY parking_id) AS temp);

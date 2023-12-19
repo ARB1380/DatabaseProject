38dc0665c096ca67fac1656d3f1346668e85469d
ARB1380 <azerila.farshi@gmail.com>
2023-12-19 21:29:32 +0330

    query 9 edited

--- phase2/9.sql
+++ phase2/9.sql
@@ -1,10 +1,35 @@
 SET SESSION my.vars.parking_Id = '123456789';
+with e as (
+      select arrival_time as dte, 1 as inc
+      from parking_receipt JOIN driving_citizen ON parking_receipt.national_code = driving_citizen.national_code
+	  WHERE driving_citizen.city_id = current_setting('my.vars.parking_Id')::char(10)
+      union all
+      select departure_time as dte, -1 as inc
+      from parking_receipt JOIN driving_citizen ON parking_receipt.national_code = driving_citizen.national_code
+	  WHERE driving_citizen.city_id = current_setting('my.vars.parking_Id')::char(10)
+     )
+select dte, next_dte, concurrent
+from (select dte, sum(sum(inc)) over (order by dte) as concurrent,
+             lead(dte) over (partition by dte) as next_dte
+      from e
+      group by dte
+     ) e
+order by concurrent desc
+fetch first 1 row only;
+	 
 
-SELECT   max_car_count, start_time, end_time
-FROM (
-    SELECT  MAX(COUNT(parking_id)) OVER() AS max_car_count, start_time, end_time
-    FROM parking_receipt
-    JOIN parking ON parking_receipt.city_Id = parking.city_Id
-    WHERE parking_id = current_setting('my.vars.parking_Id')::int
-    GROUP BY parking_id, start_time, end_time
-) subquery
+-- select dte, next_dte, concurrent
+-- from (select dte, sum(sum(inc)) over (order by dte) as concurrent,
+--              lead(dte) over (partition by dte) as next_dte
+--       from e
+--       group by dte
+--      ) e
+-- order by concurrent desc
+-- fetch first 1 row only;
+
+-- select * from driving_citizen;
+-- alter table driving_citizen
+-- add column city_id char(10);
+-- alter table driving_citizen
+-- add constraint cs foreign key(city_id) references parking(city_id);
+

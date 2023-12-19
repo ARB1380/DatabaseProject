32e184b9aa4be0ac888b4a6fe12ea55cfaacca82
zhd80 <85179695+zhd80@users.noreply.github.com>
2023-12-19 18:24:52 +0330

    Update trigger2.sql

--- phase2/trigger2.sql
+++ phase2/trigger2.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION check_citizen_credit()
 RETURNS TRIGGER AS $$
 BEGIN
     IF EXISTS (SELECT 1 FROM Citizen_Account WHERE Citizen_Id = NEW.Citizen_Id AND Credit <= 0) THEN
-        RAISE EXCEPTION 'شهروند اعتبار کافی برای استفاده از حمل و نقل عمومی و پارکینگ ندارد';
+        RAISE EXCEPTION 'Citizen does not have enough credit to use public transportation and parking';
     END IF;
     RETURN NEW;
 END;

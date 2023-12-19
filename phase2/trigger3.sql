8d994a09683ab578b05053bf861a945da64a867b
zhd80 <85179695+zhd80@users.noreply.github.com>
2023-12-19 18:26:11 +0330

    Update trigger3.sql

--- phase2/trigger3.sql
+++ phase2/trigger3.sql
@@ -1,20 +1,11 @@
--- ایجاد تریگر برای رسیدهای پارکینگ
-CREATE OR REPLACE FUNCTION generate_parking_receipt()
-RETURNS TRIGGER AS $$
-BEGIN
-  INSERT INTO parking_receipt(parking_id, arrival_time, departure_time)
-  VALUES(NEW.city_Id, NEW.start_time, NEW.end_time);
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
+-- Create a trigger to generate parking receipts
 CREATE TRIGGER trg_generate_parking_receipt
 AFTER INSERT ON parking
 FOR EACH ROW
 EXECUTE FUNCTION generate_parking_receipt();
 
 
--- ایجاد تریگر برای رسیدهای سفر
+-- Create a trigger for travel receipts
 CREATE OR REPLACE FUNCTION generate_travel_receipt()
 RETURNS TRIGGER AS $$
 BEGIN

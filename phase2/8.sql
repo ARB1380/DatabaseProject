5d19632f85d033eeb4c841cd4917f96802c29596
ARB1380 <azerila.farshi@gmail.com>
2023-12-19 20:48:16 +0330

    query 8 edited

--- phase2/8.sql
+++ phase2/8.sql
@@ -1,12 +1,14 @@
 SET SESSION my.vars.start_time = '2023-12-12 10:30:00';
 SET SESSION my.vars.end_time = '2023-12-16 10:30:00';
-SET SESSION my.vars.parking_Id = 123456789;
+SET SESSION my.vars.parking_Id = '123456789';
 
 
 SELECT COUNT(DISTINCT c.Tag) AS car_count
 FROM car c
-JOIN parking_receipt pr ON c.Tag = pr.national_Id
-WHERE pr.parking_id = current_setting('my.vars.parking_Id')::int
+JOIN parking_receipt pr ON c.Tag = pr.national_code
+WHERE pr.parking_receipt_id = current_setting('my.vars.parking_Id')::char(10)
   AND pr.arrival_time >= CAST(current_setting('my.vars.start_time') AS TIMESTAMP)
   AND pr.departure_time <= CAST(current_setting('my.vars.end_time') AS TIMESTAMP)
 GROUP BY c.Brand, c.Color;
+
+

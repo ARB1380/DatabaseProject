19539e6cc975284cbe395cf44f4af933f3022d8f
mousavi1382 <me.mousavi1382@gmail.com>
2023-12-20 06:36:09 +0330

    phase 2

--- phase2/10.sql
+++
@@ -1,9 +0,0 @@
-set session my.vars.citizen_Id = '4421045209';
-
-SELECT EXTRACT(MONTH from p.time_of_issue) AS issue_month,
-       SUM(p.payable_amount) AS total_payment
-  FROM payment_receipt p
-  JOIN citizen c ON p.citizen_national_code = c.national_code
- WHERE c.national_code = current_setting('my.vars.citizen_Id')
- GROUP BY issue_month
- ORDER BY issue_month;

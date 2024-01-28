bd306076bf0ad0c13d55879a70bcdbbcf798013d
zhd80 <85179695+zhd80@users.noreply.github.com>
2024-01-29 01:31:14 +0330

    Update CitizenSearch.py

--- phase3/CitizenSearch.py
+++ phase3/CitizenSearch.py
@@ -1,8 +1,8 @@
 from citizen import *
 
 class CitizenSearch:
-  def search_by_name(self,first_name,last_name):
+  def execute(self,args):
     result = Citizen.select().where(
-        (Citizen.first_name == first_name) & (Citizen.last_name == last_name)
+        (Citizen.first_name == args.first_name) & (Citizen.last_name == args.last_name)
     )
     return result

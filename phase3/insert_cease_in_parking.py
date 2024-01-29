216e8152750dbdca57e6a9fcd98184474a83b466
ARB1380 <azerila.farshi@gmail.com>
2024-01-29 09:49:48 +0330

    debug

--- phase3/insert_cease_in_parking.py
+++ phase3/insert_cease_in_parking.py
@@ -2,27 +2,33 @@ from parking import *
 from citizenAccount import *
 from parkingReceipt import *
 from peewee import *
+import datetime
+import math
 
 class InsertCeaseInParkingCommands:
 
     def execute(self, args):
-        query = args.departure_time - args.arrival_time
-        differentTime = query
-        cost = differentTime * args.hourly_cost
-        new_credit = CitizenAccount.credit - cost
-        
-        query1 = Parking.insert(city_id = args.city_id, parking_name = args.parking_name, arrival_time =args.arrival_time,
-                                departure_time = args.departure_time, x_location = args.x_location, y_location = args.y_location
-                                , hourly_cost = args.hourly_cost)
-        query2 = ParkingReceipt.insert(parking_receipt_id = args.parking_receipt_id, parking_city_id = args.city_id,
-                                       citizen_national_code = args.national_code, arrival_time =args.arrival_time,
-                                       departure_time = args.departure_time)
-        query3 = CitizenAccount.update(credit = new_credit)
 
-        query3 = query3.where(CitizenAccount.citizen_id == args.national_code)
-        print(args.hourly_cost.date)
-        query1.execute
-        query2.execute
-        query3.execute
-        query.execute
+        timestamp1_str = args.arrival_time
+        timestamp2_str = args.departure_time
+
+        timestamp_format = "%Y-%m-%d#%H:%M:%S"
+        timestamp1 = datetime.datetime.strptime(timestamp1_str, timestamp_format)
+        timestamp2 = datetime.datetime.strptime(timestamp2_str, timestamp_format)
+
+        time_difference = timestamp2 - timestamp1
+        hour_difference = math.ceil(time_difference.total_seconds() / 3600)
+        parking = Parking.select().where(Parking.city_id == args.parking_city_id).get()
+        hour_cost = parking.hourly_cost
+        cost = hour_difference * hour_cost
+
+        query1 = ParkingReceipt.insert(parking_receipt_id = args.parking_receipt_id, parking_city_id = args.parking_city_id,
+                                       citizen_national_code = args.citizen_national_code, arrival_time =args.arrival_time,
+                                       departure_time = args.departure_time)
+
+        query2 = CitizenAccount.update(credit = CitizenAccount.credit - cost)
+        query2 = query2.where(CitizenAccount.citizen_id == args.citizen_national_code)
+        query1.execute()
+        query2.execute()
+
         
